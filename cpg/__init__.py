487ac4bb1fb1b1c5fdb6032eb9b06b9375404891
Mogasala Hemagiri <hemagiri1394.in@gmail.com>
2026-03-21 20:18:59 +0530

    Add service boundary tracking imports and update __all__
    Added new imports and updated __all__ to include service boundary tracking components.

--- cpg/__init__.py
+++ cpg/__init__.py
@@ -1,13 +1,25 @@
-from cpg.cpg_engine import CPGEngine, get_cpg_engine
+from cpg.cpg_engine import CPGEngine, CPGBlastRadius, CPGContextSlice, get_cpg_engine
 from cpg.joern_client import JoernClient, JoernQueryError
 from cpg.program_slicer import ProgramSlicer, SliceResult, SliceDirection
 from cpg.context_selector import CPGContextSelector, ContextSlice
 from cpg.incremental_updater import IncrementalCPGUpdater, CommitDiff
+from cpg.service_boundary_tracker import (
+    ServiceBoundaryTracker,
+    ServiceCallGraph,
+    CrossServiceEdge,
+    ServiceEndpoint,
+    EdgeDirection,
+    ContractType,
+    get_service_boundary_tracker,
+)
 
 __all__ = [
-    "CPGEngine", "get_cpg_engine",
+    "CPGEngine", "CPGBlastRadius", "CPGContextSlice", "get_cpg_engine",
     "JoernClient", "JoernQueryError",
     "ProgramSlicer", "SliceResult", "SliceDirection",
     "CPGContextSelector", "ContextSlice",
     "IncrementalCPGUpdater", "CommitDiff",
+    "ServiceBoundaryTracker", "ServiceCallGraph", "CrossServiceEdge",
+    "ServiceEndpoint", "EdgeDirection", "ContractType",
+    "get_service_boundary_tracker",
 ]
